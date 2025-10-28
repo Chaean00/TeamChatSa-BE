90777e4af365c3e75efa8ae6efb0aae08edbfe31
JeongTaek <jtkim965@naver.com>
2025-10-28 17:39:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# TeamChatSa-BE
---
+++ src/test/java/com/chaean/teamchatsa/TeamChatSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaean.teamchatsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamChatSaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
