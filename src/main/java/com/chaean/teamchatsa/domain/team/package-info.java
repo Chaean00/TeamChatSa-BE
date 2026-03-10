562c6c845af4a91ce38f065f1ba7dca393802f41
JeongTaek <jtkim9965@gmail.com>
2026-03-10 17:55:48 +0900

    Refactor: 미사용 파일 정리 및 build.gradle 수정

--- src/main/java/com/chaean/teamchatsa/domain/team/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package com.chaean.teamchatsa.domain.team;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
--- src/main/java/com/chaean/teamchatsa/domain/user/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package com.chaean.teamchatsa.domain.user;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
